efaeca270e1b3d3f4dcef924ca4e233e1668d408
Remo Hertig <remo.hertig@uzh.ch>
2016-11-08 16:06:23 +0100

    load a file from web folder in DataStore

--- src/we/are/en3/server/ApplicationStartup.java
+++ src/we/are/en3/server/ApplicationStartup.java
@@ -5,6 +5,10 @@ import we.are.en3.server.DataStore;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Servlet to initialize the DataStore on application startup
@@ -18,7 +22,10 @@ public class ApplicationStartup implements ServletContextListener {
     public void contextInitialized(ServletContextEvent event) {
         System.out.println("initialize servlet");
 
-        DataStore.getInstance().loadCSVFile("correct path...");
+        // open a file from the web/ folder
+        InputStream input = event.getServletContext().getResourceAsStream("/MyClimate.css");
+        DataStore.getInstance().loadCSVFile(input);
+
     }
 
     @Override
--- src/we/are/en3/server/DataStore.java
+++ src/we/are/en3/server/DataStore.java
@@ -1,14 +1,28 @@
 package we.are.en3.server;
 
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class DataStore {
 
     ArrayList<String> ls = new ArrayList<String>();
 
-    public void loadCSVFile(String path) {
+    public void loadCSVFile(InputStream input) {
         System.out.println("loadCSVFile...");
+
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+        String line;
+        try {
+            while((line=reader.readLine())!=null){
+                System.out.println(line);
+            }
+        } catch (Throwable ignore) {}
+
 
         ls.add("foo");
         ls.add("bar");
