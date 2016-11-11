f50b29bef3afd74001bd3fab918127c5bf521e00
since4 <rol123ben@outlook.de>
2016-11-11 21:41:10 +0100

    added a cityArrayList countryArrayList and areaToyearsMap for dropdown lists in table filter

--- src/we/are/en3/server/DataStore.java
+++ src/we/are/en3/server/DataStore.java
@@ -4,8 +4,8 @@ package we.are.en3.server;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.lang.reflect.Array;
+import java.util.*;
 
 import we.are.en3.client.model.DataPoint;
 
@@ -22,10 +22,15 @@ import we.are.en3.client.model.DataPoint;
  */
 public class DataStore {
 
-    //Hash map
+    //Area Hash map
     // with keys: countries and cities (defined as areas)
     // with values: array of all DataPoint's for one area
     HashMap<String,ArrayList<DataPoint>> areaMap = new HashMap<String, ArrayList<DataPoint>>();
+
+    //Data Structures for Dropdown Lists
+    ArrayList citySortedList;
+    ArrayList countrySortedList;
+    HashMap<String,ArrayList<String>> areaToYearMap = new HashMap<String,ArrayList<String>>();
 
     /**
      * Method that loads the CSV file from tha app's web folder
@@ -34,16 +39,21 @@ public class DataStore {
      * @param
      * @return
      */
+    @SuppressWarnings("Duplicates")
     public void loadCSVFile(InputStream input) {
 
         //Information for Database Administrator
         System.out.println("loadCSVFile...");
 
-        //Line reader that reads that connects to the csv-file.
+        //Line reader that connects to the csv-file.
         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
         //Line returned by the BufferedReader
         String line;
+
+        //Sets to generate dropdown lists
+        HashSet<String> citySet = new HashSet<String>(100);
+        HashSet<String> countrySet = new HashSet<String>(100);
 
         try {
 
@@ -67,6 +77,22 @@ public class DataStore {
                 String Latitude=arr[5];
                 String Longitude=arr[6];
 
+                //extract year
+                String dateSplitArr[] = dt.split("-");
+                String year = dateSplitArr[0];
+
+                /*
+                * Generate Area Sets
+                * */
+
+                citySet.add(City);
+                countrySet.add(Country);
+
+
+                /*
+                * Generate Area HashMaps
+                * */
+
                 //construct a data point: one line/record of the csv-file
                 DataPoint dp = new DataPoint(
                         dt,
@@ -80,38 +106,82 @@ public class DataStore {
                 //areaKey: during loop it will take on the value of cities and countries
                 String areaKey;
 
-                //references data point objects to countries
-                areaKey=Country;
-
                 //The first time a new area=Country is encountered
                 // it "puts" a (key, value) - pair to the areaMap
-                // key=area, value=ArrayList of DataPoints
+                // key=area, value=ArrayList of DataPoints or Strings
+                areaKey=Country;
+
                 if (!areaMap.containsKey(areaKey)){
-                    ArrayList<DataPoint> arrList = new ArrayList<DataPoint>();
-                    areaMap.put(areaKey, arrList);
+                    ArrayList<DataPoint> arr1List = new ArrayList<DataPoint>();
+                    ArrayList<String> arr2List = new ArrayList<String>();
+                    areaMap.put(areaKey, arr1List);
+                    areaToYearMap.put(areaKey,arr2List);
                 }
 
                 //"adds" a new DataPoint object reference to the ArrayList
                 areaMap.get(areaKey).add(dp);
 
+                //"adds" a year to the ArrayList
+                Integer lastIndex = areaToYearMap.get(areaKey).size();
+                if (lastIndex!=0){
+                    String lastElement = areaToYearMap.get(areaKey).get(lastIndex-1);
+                    if (!lastElement.contains(year)) {
+                        areaToYearMap.get(areaKey).add(year);
+                    }
+                }else{
+                    areaToYearMap.get(areaKey).add(year);
+                }
                 //The first time a new area=City is encountered
                 // it "puts" a (key, value) - pair to the areaMap
-                // key=area, value=ArrayList of DataPoints
+                // key=area, value=ArrayList of DataPoints or Strings
                 areaKey=City;
+
                 if (!areaMap.containsKey(areaKey)){
-                    ArrayList<DataPoint> arrList = new ArrayList<DataPoint>();
-                    areaMap.put(areaKey, arrList);
+                    ArrayList<DataPoint> arr3List = new ArrayList<DataPoint>();
+                    ArrayList<String> arr4List = new ArrayList<String>();
+                    areaMap.put(areaKey, arr3List);
+                    areaToYearMap.put(areaKey,arr4List);
                 }
 
                 //"adds" a new DataPoint object reference to the ArrayList
                 areaMap.get(areaKey).add(dp);
+
+                //"adds" a year to the ArrayList if not there yet
+                lastIndex = areaToYearMap.get(areaKey).size();
+                if (lastIndex!=0) {
+                    String lastElement = areaToYearMap.get(areaKey).get(lastIndex-1);
+                    if (!lastElement.contains(year)) {
+                        areaToYearMap.get(areaKey).add(year);
+                    }
+                }else{
+                    areaToYearMap.get(areaKey).add(year);
+                }
 
             }
 
             //System.out.println(cityMap);
 
-        } catch (Throwable ignore) {}
+            //Convert Sets into sorted ArrayLists
+            this.citySortedList = new ArrayList<String>(citySet);
+            Collections.sort(citySortedList);
+            this.countrySortedList = new ArrayList<String>(countrySet);
+            Collections.sort(countrySortedList);
 
+        }
+        catch (Throwable ignore) {}
+        finally {
+            //Information for Database Administrator
+            System.out.println("loadCSVFile...print areaToYearMap");
+            System.out.println(Arrays.toString(areaToYearMap.get("London").toArray()));
+            System.out.println("loadCSVFile...print citySortedList");
+            System.out.println(Arrays.toString(this.citySortedList.toArray()));
+            System.out.println("loadCSVFile...print countrySortedList");
+            System.out.println(Arrays.toString(this.countrySortedList.toArray()));
+            System.out.println("loadCSVFile...print again countrySortedList");
+            for(Object s : this.countrySortedList) {
+                System.out.println(s);
+            }
+        }
 
     }
 
